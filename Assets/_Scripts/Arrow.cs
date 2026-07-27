698a40c47b9844c3ece007581ff0d3d295417385
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2026-07-28 03:49:52 +0900

    [update]弓矢が進行方向を向くように修正

--- Assets/_Scripts/Arrow.cs
+++ Assets/_Scripts/Arrow.cs
@@ -2,6 +2,20 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Arrow : MonoBehaviour
 {
+    [Header("-----参照-----")]
+    [SerializeField] private Transform _tr;
     [SerializeField] private Rigidbody2D _rb;
 
+    [Header("-----パラメータ調整-----")]
+    [SerializeField] private float _angleOffset = 90f;
+
+    private void Update()
+    {
+        if(_rb.linearVelocity.sqrMagnitude > 0.01f)
+        {
+            _tr.rotation = Quaternion.Euler(0f, 0f, 
+                Mathf.Atan2(_rb.linearVelocity.y,_rb.linearVelocity.x)
+                * Mathf.Rad2Deg + _angleOffset);
+        }
+    }
 }
