561fad74c2080ee860598395af3476ab390e4325
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2026-08-02 02:18:46 +0900

    [update]インクが複数個飛び散るようにした

--- Assets/_Scripts/InkManager.cs
+++ Assets/_Scripts/InkManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InkManager : MonoBehaviour
@@ -10,17 +11,36 @@ public class InkManager : MonoBehaviour
     [Header("-----パラメータ調整-----")]
     [SerializeField] private Color[] _colors;
     [SerializeField] private Sprite[] _sprites;
+    [SerializeField] private float _minScaleMag = 0.8f;
+    [SerializeField] private float _maxScaleMag = 1.5f;
+    [SerializeField] private float _scatter = 1.5f;
     private void Awake()
     {
         Instance = this;
     }
     public void DisplayInk(Vector3 pos)
     {
-        pos += new Vector3(0f, 0f, 1f);
-        GameObject ink = Instantiate(_prefab, pos,
-            Quaternion.Euler(0, 0, Random.Range(0f, 360f)),_parent);
-        SpriteRenderer sr = ink.GetComponent<SpriteRenderer>();
-        sr.sprite = _sprites[Random.Range(0, _sprites.Length)];
-        sr.color = _colors[Random.Range(0, _colors.Length)];
+        int amount = Random.Range(3,5);
+        List<Color> colors = new List<Color>(_colors);
+
+        for(int i  = 0; i < amount; i++)
+        {
+            //座標計算
+            float x = Random.Range(-_scatter, _scatter);
+            float y = Random.Range(-_scatter, _scatter);
+            Vector3 spawnPos = pos + new Vector3(x, y, 1f);
+
+            GameObject ink = Instantiate(_prefab, spawnPos,
+                Quaternion.Euler(0, 0, Random.Range(0f, 360f)), _parent);
+            ink.transform.localScale *= Random.Range(_minScaleMag, _maxScaleMag);
+
+            SpriteRenderer sr = ink.GetComponent<SpriteRenderer>();
+            sr.sprite = _sprites[Random.Range(0, _sprites.Length)];
+
+            //色計算
+            int index = Random.Range(0, colors.Count);
+            sr.color = colors[index];
+            colors.RemoveAt(index);
+        }
     }
 }
